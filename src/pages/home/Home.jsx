6a2dd9e3dbed50bf2d74615258c9934ce5895688
ChristopherCalderon <chris2001289@gmail.com>
2024-03-12 18:46:52 -0600

    feat: add responsive footer :sparkles:

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
 
 function Home() {
   return (
-    <Navbar />
+    <div className='min-h-screen h-screen'>
+<Navbar />
+    <Footer />
+    </div>
+    
   )
 }
 
