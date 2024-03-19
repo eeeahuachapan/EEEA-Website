d1a5815367c814fd3138f7b661e763598e5cc210
ChristopherCalderon <chris2001289@gmail.com>
2024-03-18 20:12:51 -0600

    feat: add mobile events page :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Facilities from "./pages/facilities/Facilities"
-import AboutUs from "./pages/aboutUs/AboutUs"
 import Home from "./pages/home/Home"
 import Calendar from "./pages/calendar/Calendar"
+import AboutUs from "./pages/aboutus/AboutUs"
+import Events from "./pages/events/Events"
+
 
 function App() {
 
   return (
     <>
-      <Calendar />
+      <Events />
     </>
   )
 }
