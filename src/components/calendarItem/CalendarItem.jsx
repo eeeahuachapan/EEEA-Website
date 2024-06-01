0d4652ee09336b3e7a2e42f8f8474a007866b2e9
moralesmajo <00155020@uca.edu.sv>
2024-05-31 21:36:28 -0600

    feat:get all activities on calendar :sparkles:

--- src/components/calendarItem/CalendarItem.jsx
+++ src/components/calendarItem/CalendarItem.jsx
@@ -1,12 +1,30 @@
-function CalendarItem() {
+function CalendarItem({ activity }) {
+    const activityDate = new Date(activity.datetime);
+    const formattedDate = activityDate.toLocaleDateString('es-ES', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    });
+
+    const formattedTime = activityDate.toLocaleTimeString('es-ES', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+      });
+
     return (
         <div className="card card-side bg-base-100 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] h-fit -z-10 font-Montserrat">
-            <div className="flex items-center rounded-l-xl justify-center text-xs lg:text-sm text-center font-semibold p-3 w-52 lg:w-40 bg-accent text-white">30 de Septiembre</div>
+            <div className="flex items-center rounded-l-xl justify-center text-xs lg:text-sm text-center font-semibold p-3 w-28 lg:w-56 bg-accent text-white">
+                {formattedDate}
+            </div>
             <div className="card-body rounded-r-xl text-xs lg:text-sm bg-white">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <p>{activity.title}</p>
+                {!activity.isAllDay && (
+                    <p className="text-accent font-semibold">Hora: {formattedTime}</p>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
 export default CalendarItem
--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,8 +1,67 @@
-import Navbar from "../../components/navbar/Navbar"
-import Footer from "../../components/footer/Footer"
-import CalendarItem from "../../components/calendarItem/CalendarItem"
+import { getAllActivities, getActivitiesByMonth } from "../../services/calendarServices";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import CalendarItem from "../../components/calendarItem/CalendarItem";
+import { useEffect, useState } from "react";
+import Loader from "../../components/loader/Loader";
 
 function Calendar() {
+    const [loading, setLoading] = useState(false);
+    const [activities, setActivities] = useState([]);
+    const [month, setMonth] = useState(null);
+
+    const getAllData = async () => {
+        try {
+            setLoading(true);
+            const response = await getAllActivities();
+          
+            if (response) {
+                setActivities(response.data.activities);
+                console.log("Activities fetched:", response.data.activities);
+            }
+            setLoading(false);
+        } catch (error) {
+            console.error('Error al obtener datos de la API:', error);
+            setLoading(false);
+        }
+    };
+
+    const getMonthData = async () => {
+        try {
+            setLoading(true);
+            const response = await getActivitiesByMonth( month );
+          
+            if (response) {
+                setActivities(response.data);
+                console.log("Activities fetched:", response.data);
+            }
+            setLoading(false);
+        } catch (error) {
+            console.error('Error al obtener datos de la API:', error);
+            setLoading(false);
+        }
+    };
+
+    const toggleMonth = (num) => {
+        console.log("Toggling month. Current month:", month, " New month:", num);
+        if (month === num) {
+            setMonth(null);
+            console.log("Month set to null");
+            return;
+        } else {
+            setMonth(num);
+            console.log("Month set to:", num);
+        }
+    };
+
+    useEffect(() => {
+        if (month === null) {
+            getAllData();
+        } else {
+            getMonthData();
+        }
+    }, [month]);
+
     return (
         <div className="h-screen min-h-screen flex flex-col items-center justify-between font-Montserrat">
             <Navbar />
@@ -11,34 +70,42 @@ function Calendar() {
 
             <section className="w-full lg:h-3/4 flex flex-col lg:flex-row">
                 <div className="flex flex-wrap lg:w-40 justify-center join join-horizontal lg:join-vertical mx-4 p-6 gap-1">
-                    <button className="btn btn-sm border-white">Enero</button>
-                    <button className="btn btn-sm border-white">Febrero</button>
-                    <button className="btn btn-sm border-white">Marzo</button>
-                    <button className="btn btn-sm border-white">Abril</button>
-                    <button className="btn btn-sm border-white">Mayo</button>
-                    <button className="btn btn-sm border-white">Junio</button>
-                    <button className="btn btn-sm border-white">Julio</button>
-                    <button className="btn btn-sm border-white">Agosto</button>
-                    <button className="btn btn-sm border-white">Septiembre</button>
-                    <button className="btn btn-sm border-white">Octubre</button>
-                    <button className="btn btn-sm border-white">Noviembre</button>
-                    <button className="btn btn-sm border-white">Diciembre</button>
+                    <button onClick={() => toggleMonth(1)} className={`btn btn-sm border-white ${month === 1 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Enero</button>
+                    <button onClick={() => toggleMonth(2)} className={`btn btn-sm border-white ${month === 2 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Febrero</button>
+                    <button onClick={() => toggleMonth(3)} className={`btn btn-sm border-white ${month === 3 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Marzo</button>
+                    <button onClick={() => toggleMonth(4)} className={`btn btn-sm border-white ${month === 4 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Abril</button>
+                    <button onClick={() => toggleMonth(5)} className={`btn btn-sm border-white ${month === 5 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Mayo</button>
+                    <button onClick={() => toggleMonth(6)} className={`btn btn-sm border-white ${month === 6 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Junio</button>
+                    <button onClick={() => toggleMonth(7)} className={`btn btn-sm border-white ${month === 7 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Julio</button>
+                    <button onClick={() => toggleMonth(8)} className={`btn btn-sm border-white ${month === 8 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Agosto</button>
+                    <button onClick={() => toggleMonth(9)} className={`btn btn-sm border-white ${month === 9 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Septiembre</button>
+                    <button onClick={() => toggleMonth(10)} className={`btn btn-sm border-white ${month === 10 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Octubre</button>
+                    <button onClick={() => toggleMonth(11)} className={`btn btn-sm border-white ${month === 11 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Noviembre</button>
+                    <button onClick={() => toggleMonth(12)} className={`btn btn-sm border-white ${month === 12 ? 'bg-accent text-white hover:bg-accent' : ''}`}>Diciembre</button>
                 </div>
 
-                <div className="flex flex-col lg:w-11/12 gap-6 p-6 lg:overflow-y-auto lg:bg-shapes lg:z-0 ">
-                    <CalendarItem />
-                    <CalendarItem />
-                    <CalendarItem />
-                    <CalendarItem />
-                    <CalendarItem />
-                    <CalendarItem />
-                    <CalendarItem />
+                <div className="flex flex-col lg:w-11/12 gap-6 p-6 lg:overflow-y-auto lg:bg-shapes lg:z-0">
+                    {loading && (
+                        <div className="w-full h-full flex items-center justify-center">
+                            <Loader />
+                        </div>
+                    )}
+
+                    {activities.length === 0 && !loading ? (
+                        <div className="w-full flex flex-col lg:justify-center items-center gap-5 h-full">
+                            <h1 className="text-center text-3xl text-primary font-bold">No se encontraron actividades</h1>
+                        </div>
+                    ) : (
+                        activities.map((activity) => (
+                            <CalendarItem key={activity.id} activity={activity} />
+                        ))
+                    )}
                 </div>
             </section>
 
             <Footer />
         </div>
-    )
+    );
 }
 
 export default Calendar
