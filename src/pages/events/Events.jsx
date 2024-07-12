3e54ca638b0ce980e6d69257ccb12242bbfba532
moralesmajo <00155020@uca.edu.sv>
2024-07-11 18:14:24 -0600

    fix: bad alignment in 'events' screen

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -112,7 +112,7 @@ function Events() {
                     <button onClick={() => toggleMonth(12)} className={`text-xs btn btn-xs border-white ${month === 12 ? 'bg-accent text-white hover:border-black hover:bg-accent' : ''}`}>Diciembre</button>
                 </div>
 
-                <div className="flex flex-col lg:flex-row w-full lg:w-11/12 lg:flex-wrap lg:justify-between items-center gap-6 p-6 lg:overflow-y-auto lg:bg-shapes lg:z-0">
+                <div className="flex flex-col lg:flex-row w-full min-h-screen lg:min-h-max lg:w-11/12 lg:flex-wrap lg:justify-between lg:items-stretch items-center gap-6 p-6 lg:overflow-y-auto lg:bg-shapes lg:z-0">
 
 
                     {loading && (
