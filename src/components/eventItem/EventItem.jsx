54f575dc89eedcd799c94e5c0ce08e2380f6624c
ChristopherCalderon <chris2001289@gmail.com>
2024-07-11 21:51:16 -0600

    fix: Completed icon not showing :bug:

--- src/components/eventItem/EventItem.jsx
+++ src/components/eventItem/EventItem.jsx
@@ -42,7 +42,7 @@ function EventItem({ event }) {
             <p className="lg:w-44 w-32">{formattedTime}</p></label>
           <label className='flex flex-row gap-1 items-center text-left'><svg width="64px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#f4676a" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
             <p className="lg:w-44 w-32">{event.location}</p></label>
-          {event.completed && (<img src="/src/assets/done.webp" className="w-8 lg:w-9 absolute right-0 bottom-0" alt="" />)}
+          {event.completed && (<img src="assets/done.webp" className="w-8 lg:w-9 absolute right-0 bottom-0" alt="" />)}
 
         </div>
       </div>
